b13150f7a7f7815f9d4e84af88ec5db14e936b70
Adeola Jonathan <eyeslikejays@yahoo.com>
2022-09-16 15:03:59 +0100

    in

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,83 +1,46 @@
 #include "main.h"
-#include <limits.h>
 
 /**
- * num_digits - returns the number of digits in a decimal number
- *@n: - number that is analyzed for number of digits it contains
- * Return: num = number of digits
- **/
-
-int num_digits(int n)
-{
-	int num = 0;
-
-	if (n == 0)
-		num = 1;
-	else
-		while (n != 0)
-		{
-			num++;
-			n /= 10;
-		}
-	return (num);
-}
-
-/**
- * posPower - Positive Power! calculates abs(x to the y), yo!
- * @x: the x in x to the y
- * @y: the y in x to the y
- * Return: - num (calculated value of abs(x to the y))
- **/
-int posPower(int x, int y)
-{
-	int val, i;
-
-	if (y > 0)
-	{
-		if (x < 0)
-			x = -x;
-		val = 1;
-		for (i = 0; i < y; i++)
-			val = val * x;
-		return (val);
-	}
-	else
-		return (1);
-}
-/**
- * print_number - print a number that is passed in
- * INT_MIN  INT_MAX
- * @n: - number that is passed in to print
- * return:  void
- **/
-
+ * print_number - prints number
+ *
+ * @n: integer to print to character
+ */
 void print_number(int n)
 {
-	int i, x, digits;
-	int oneD;
-	char minus = '+';
-	int revNumber = 0;
+	int i;
+	int d = 1;
+	unsigned int x = n;
+	unsigned int y = n;
+	int c = 0;
 
-	if (n < 0)
-		minus = '-';
-	else
-		n = -n;
-	digits = num_digits(n);
-	for (i = digits - 1; i >= 0; i--)
+	if (n == 0)
 	{
-		oneD = n % 10;
-		n = n / 10;
-		revNumber = posPower(10, i) * oneD + revNumber;
-	}
-	if (revNumber == 0)
 		_putchar('0');
-	else if (minus == '-')
-		_putchar('-');
-	x = 0;
-	while (revNumber != 0)
+	}
+	if (n < 0)
 	{
-		x = revNumber % 10;
-		_putchar('0' - x);
-		revNumber /= 10;
+		_putchar('-');
+		n = n + 1;
+		n = -n;
+		y = n;
+		x = n;
+		x += 1;
+		y += 1;
+	}
+
+	while (x != 0)
+	{
+		x = x / 10;
+		c++;
+	}
+	for (i = 1; i < c; i++)
+	{
+		d *= 10;
+	}
+	for (i = 0; i < c; i++)
+	{
+		_putchar(y / d + '0');
+		y = y % d;
+		d = d / 10;
 	}
 }
