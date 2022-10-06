fee70fb5a3c01878223572f24c54e43b4dcca9a3
Adeola Jonathan <eyeslikejays@yahoo.com>
2022-10-06 02:04:49 +0100

    o

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,37 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#define UNUSED(x) (void)(x)
-/**
- * StringCheck - checks string
- * @s: string to check
- * Return: boolean
- */
-int StringCheck(char *s)
-{
-	int i = 0;
 
-	for (; s[i] != '\0'; i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
  */
-int main(int argc, char  *argv[])
-{
-	int i;
-	int result = 0;
 
-	if (argc > 1)
+int main(int argc, char **argv)
+{
+	int i, j, sum = 0;
+	char *flag;
+
+	if (argc < 2)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (StringCheck(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", result);
+		printf("0\n");
 		return (0);
 	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
 
+	for (i = 1; argv[i]; i++)
+	{
+		j = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += j;
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
